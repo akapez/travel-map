8555dab91d05bd7b364e4197ecba985cebbd5915
avishka964 <avishkakapuruge@gmail.com>
2021-06-16 17:35:56 +0530

    prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
+import path from "path"
 import express from 'express'
 import dotenv from 'dotenv'
-import morgan from "morgan"
+import morgan from 'morgan'
 import connectDB from './config/db.js'
-import {notFound, errorHandler} from './middleware/errorMiddleware.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import pinRoutes from './routes/pinRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 //load env
@@ -14,17 +15,27 @@ const app = express()
 
 app.use(express.json())
 
-if (process.env.NODE_ENV === "development") {
-    app.use(morgan("dev"))
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
 }
 
 //routes
 app.use('/api/pin', pinRoutes)
 app.use('/api/users', userRoutes)
 
-app.get('/', (req, res) => {
-    res.send('API is running...')
-})
+const __dirname = path.resolve()
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/build")))
+  
+    app.get("*", (req, res) =>
+      res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    )
+  } else {
+    app.get("/", (req, res) => {
+      res.send("API is running")
+    })
+  }
 
 //error middleware
 app.use(notFound)
@@ -32,4 +43,7 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
